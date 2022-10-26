02e5eea351c54906766867380cbb423c4d6d62b2
Jean Felipe Sorio <jeanfsorio@gmail.com>
2022-10-26 16:57:30 -0300

    loop 10 vezes para teste

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import psycopg2
 import requests
 from datetime import datetime
+import time
 
-#fetch form url
-url = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
-response = requests.get(url)
-
-
-#establishing the connection
-conn = psycopg2.connect(
-   "postgresql://postgres:EKoXvG1FI2gQe64rpZZg@containers-us-west-27.railway.app:6706/railway"
-)
-cursor = conn.cursor()
-cursor.execute( "INSERT INTO eleicao_log (apuracao, horario) " + 
-                      f"VALUES ('{response.text}', '{datetime.now()}');")
-conn.commit()
-conn.close()
-
-
-
-print("Done!")
+def loop():   
+   #fetch form url
+   url = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
+   response = requests.get(url)
+   #establishing the connection
+   conn = psycopg2.connect("postgresql://postgres:EKoXvG1FI2gQe64rpZZg@containers-us-west-27.railway.app:6706/railway")
+   cursor = conn.cursor()
+   cursor.execute( "INSERT INTO eleicao_log (apuracao, horario) " + 
+                        f"VALUES ('{response.text}', '{datetime.now()}');")
+   conn.commit()
+   conn.close()
+   print(f"Done! - {datetime.now()}")
+   time.sleep(600)
+   
+for x in range(0,10):
+   loop()
